63c00cd0e4a04a135f4d79d4613ac7893ab82fc3
Anatolii <anatolii.melchenko@academysmart.com>
2022-12-06 13:33:48 +0200

    add basic user input handling;

--- src/JobExecutor.java
+++ src/JobExecutor.java
@@ -4,11 +4,27 @@ import java.util.List;
 public class JobExecutor {
 
     private List<Job> periodicJobs = new ArrayList();
-    private List<Job> oneTimeJobs = new ArrayList();
+    private List<Job> singleJobs = new ArrayList();
+
+    public List<Job> getPeriodicJobs() {
+        return periodicJobs;
+    }
+
+    public void setPeriodicJobs(List<Job> periodicJobs) {
+        this.periodicJobs = periodicJobs;
+    }
+
+    public List<Job> getSingleJobs() {
+        return singleJobs;
+    }
+
+    public void setSingleJobs(List<Job> singleJobs) {
+        this.singleJobs = singleJobs;
+    }
 
     public void addJob(Job job, JobType jobType) {
         if (jobType.equals(JobType.SINGLE)) {
-            this.oneTimeJobs.add(job);
+            this.singleJobs.add(job);
         } else if (jobType.equals(JobType.PERIODIC)) {
             this.periodicJobs.add(job);
         } else {
@@ -21,10 +37,14 @@ public class JobExecutor {
     }
 
     public void startJobs() {
-        for (Job oneTimeJob : oneTimeJobs) {
+        for (Job oneTimeJob : singleJobs) {
             Thread thread = new Thread(oneTimeJob);
             thread.start();
         }
+    }
+
+    public boolean isEmpty() {
+        return singleJobs.isEmpty() && periodicJobs.isEmpty();
     }
 
 }
--- src/JobSystemMain.java
+++ src/JobSystemMain.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class JobSystemMain {
 
     public static void main(String[] args) {
+        boolean programLaunched = true;
+
         JobExecutor executor = new JobExecutor();
         Job job1 = new Job("JOB_1");
         Job job2 = new Job("JOB_2");
@@ -9,8 +14,30 @@ public class JobSystemMain {
         executor.addJob(job1, JobType.SINGLE);
         executor.addJob(job2, JobType.SINGLE);
 
-        System.out.println("Starting jobs...");
-        executor.startJobs();
+        while (programLaunched) {
+            printJobInfo(executor);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                String input = reader.readLine();
+                if ("Q".equalsIgnoreCase(input)) {
+                    programLaunched = false; // close program
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void printJobInfo(JobExecutor executor) {
+        System.out.println("JOB INFO:");
+        if (executor.isEmpty()) {
+            System.out.println("No active jobs found");
+        } else {
+            System.out.println("Single jobs (name - state - start time)");
+            for (Job job : executor.getSingleJobs()) {
+                System.out.println(job.getName() + " - " + job.getJobState() + " - " + job.getStartTime());
+            }
+        }
     }
 
 }
